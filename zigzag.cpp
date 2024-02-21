bc29ec281c09eba2de260c0ed38c65789da94cf8
Colten Mikulastik <mikulastikc@gmail.com>
2024-02-20 18:16:40 -0600

    moved the function into a class

--- zigzag.cpp
+++ zigzag.cpp
@@ -2,55 +2,73 @@
 #include <iostream>
 #include <string>
 
-void print_zig_string(std::string *str, int k)
+
+class Solution
 {
+	private:
+		std::string input;
+		std::string *zig_stuff;
+		int k;
+	public:
+		Solution(std::string t_input, int t_k)
+		{
+			this->input = t_input;
+			this->k = t_k;
+			this->zig_stuff = new std::string[t_k];
+			
+			// fill zig_stuff string with blank chars
+			for(int i{}; i <= (k - 1); i++)	
+				for (int j{}; j <= (t_input.length() / 2); j++)
+					this->zig_stuff[i].push_back(' ');
+		}
 
-	for( int i{}; i <= (k-1); i++)
-	{
-		std::cout << str[i] << std::endl;
-	}
+		bool zig_n_zag()
+		{
+			int index_x {}, index_y {};
+			bool do_go_up = false;
 
-}
+			for(char character : this->input)
+			{
+				this->zig_stuff[index_x][index_y] = character;
+				if(index_x == 0)
+				{
+					do_go_up = false;
+				}
+				if(index_x == (k - 1) || do_go_up)
+				{
+					index_x--;
+					index_y++;
+					do_go_up = true;
+				}
+				else
+				{
+					index_x++;
+				}
+			}
+			return true;
+		}
+
+		void print_zig_string()
+		{
+			for( int i{}; i <= (this->k-1); i++)
+			{
+				std::cout << this->zig_stuff[i] << std::endl;
+			}
+
+		}
+
+		~Solution()
+		{
+			delete [] this->zig_stuff;
+		}
+};
 
 
 int main()
 {
-	std::string input = "this is a test";
-	int k = 3;
-	std::string *test = new std::string[k];
-	
-
-	std::cout << input << std::endl;
-
-	for(int i{}; i <= (k - 1); i++)	
-		for (int j{}; j <= (input.length() / 2); j++)
-			test[i].push_back(' ');
-
-	print_zig_string(test, k);
-
-	int ind_x {}, ind_y {};
-	bool do_go_up = 0;
-
-	for(char character : input)
-	{
-		print_zig_string(test, k);
-		test[ind_x][ind_y] = character;
-		if(ind_x == 0)
-		{
-			do_go_up = 0;
-		}
-		if(ind_x == (k - 1) || do_go_up)
-		{
-			ind_x--;
-			ind_y++;
-			do_go_up = 1;
-		}
-		else 
-		{
-			ind_x++;
-		}
-	}
-	print_zig_string(test, k);
-
-	delete [] test;
+	std::string input = "big ups the whole island, masive its ya boy chet\"ina\" boyaka boyaka errrrrr";
+	int k = 5;
+	Solution test(input, k);
+	test.zig_n_zag();
+	test.print_zig_string();
 }
