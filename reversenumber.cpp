176126054ee157926cd73efa93f7169afc407c0c
Colten Mikulastik <mikulastikc@gmail.com>
2024-02-22 19:13:24 -0600

    working on reverse numbers program in cpp

---
+++ reversenumber.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+
+// input of function int = 12345
+// output of function int = 54321
+
+
+int reverse(int in)
+{
+	int out {};
+	double decimal_places {};
+
+	do 
+	{
+		// decimal_places = in / 10;
+	}
+	while (decimal_places != 0);
+
+	out = decimal_places;
+	return out;
+}
+
+int main()
+{
+	int val = 111;
+	
+	std::cout << val / 2 << std::endl;
+	// std::cout << reverse(val) << std::endl;
+
+	return 0;
+}
