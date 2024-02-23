e228d99d41bb388c72b1484e922be9bdd8427445
Colten Mikulastik <mikulastikc@gmail.com>
2024-02-22 20:51:40 -0600

    bad but somewhat working solution to reversing a number

--- reversenumber.cpp
+++ reversenumber.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <cmath>
+
 
 // input of function int = 12345
 // output of function int = 54321
@@ -7,25 +9,41 @@
 
 int reverse(int in)
 {
-	int out {};
-	double decimal_places {};
+	int temp {in}, out {}, decimal_places {};
 
 	do 
 	{
-		// decimal_places = in / 10;
+		decimal_places++;
+		temp = temp / 10;
 	}
-	while (decimal_places != 0);
+	while (temp != 0);
+	std::cout << "decimal places: " << decimal_places << std::endl;
 
-	out = decimal_places;
-	return out;
+	int real_place {};
+	for(int i{decimal_places}; i >= 0; i--)
+	{
+		int place_val = in / pow(10, i);
+		in = in - (place_val * pow(10,i));
+		std::cout << "input value now: " << in << std::endl;
+		std::cout << "place value found: " << place_val << std::endl;
+		out = (place_val * pow(10,real_place)) + out;
+		real_place++;
+		std::cout << "out is : " << out << std::endl;
+		std::cout << "real place is: " << real_place << std::endl;
+		std::cout << "--------------"<< std::endl;
+	}
+	return out / 10;
 }
 
 int main()
 {
-	int val = 111;
-	
-	std::cout << val / 2 << std::endl;
-	// std::cout << reverse(val) << std::endl;
+	while(true)
+	{
+		int val;
+		std::cout << "input variable to test: ";
+		std::cin >> val;
 
+		std::cout << reverse(val) << std::endl;
+	}
 	return 0;
 }
