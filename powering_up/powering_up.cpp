7d5865b31969921af43471af0c5f436fcee1af29
ColtenMikulastik <mikulastikc@gmail.com>
2024-05-16 18:49:51 -0500

    working solution in main

--- powering_up/powering_up.cpp
+++ powering_up/powering_up.cpp
@@ -4,16 +4,19 @@
 int powerup();
 
 
-int powerup(double x, int n)
+double powerup(double x, int n)
 {
-	double product {};
-
-
+	double product {1};
+	for(int i{}; i < n; i++)
+	{
+		std::cout << "multiplying " << product << " by " << x << " for the " << i+1 << "th time" << std::endl;
+		product = product * x;
+	}
+	return product;
 }
 
 int main()
 {
-	
-
+	std::cout << powerup(3, 2);
 	return 0;
 }
