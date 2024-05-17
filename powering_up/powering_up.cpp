ffde381c8d91d9341b28c51c633d50f281e737d2
ColtenMikulastik <mikulastikc@gmail.com>
2024-05-16 19:04:17 -0500

    finished answer for power

--- powering_up/powering_up.cpp
+++ powering_up/powering_up.cpp
@@ -1,22 +1,34 @@
 
 #include <iostream>
 
-int powerup();
-
-
-double powerup(double x, int n)
+class Solution
 {
-	double product {1};
-	for(int i{}; i < n; i++)
+public:
+	double powerup(double x, int n)
 	{
-		std::cout << "multiplying " << product << " by " << x << " for the " << i+1 << "th time" << std::endl;
-		product = product * x;
+		double product {1};
+		for(int i{}; i < n; i++)
+		{
+			std::cout << "multiplying " << product << " by " << x << " for the " << i+1 << "th time" << std::endl;
+			product = product * x;
+		}
+		return product;
 	}
-	return product;
-}
+
+	Solution(){}
+	~Solution(){}
+};
+
 
 int main()
 {
-	std::cout << powerup(3, 2);
+	int power {};
+	Solution test;
+	double base {};
+	std::cout << "give base number: ";
+	std::cin >> base;
+	std::cout << "give a power: ";
+	std::cin >> power;
+	std::cout << test.powerup(base, power) << std::endl;
 	return 0;
 }
