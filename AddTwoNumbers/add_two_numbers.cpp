9761baad5633288fd95ab819a11ca077dcd9d4c0
Colten Mikulastik <mikulastikc@gmail.com>
2024-07-07 17:32:01 -0500

    reinventing the wheel for the linked lists

--- AddTwoNumbers/add_two_numbers.cpp
+++ AddTwoNumbers/add_two_numbers.cpp
@@ -1,13 +1,57 @@
 
 // luca's stuff
 
+class Node 
+{
+	private:
+		Node *next_node {};
+		int value {};
+	public:
+		Node(int value, Node *next_node=nullptr)
+		{
+			this->value=value;
+			this->next_node=next_node;
+		}
+		void set_value(int t_value)
+		{
+			this->value = t_value;
+		}
+
+		void set_next_node(Node *t_next_node)
+		{
+			this->next_node = t_next_node;
+		}
+		
+		int get_value()
+		{	
+			return this->value;
+		}
+		Node *get_next_node()
+		{
+			return this->next_node;
+		}
+
+};
+
+
 #include <iostream>
-#include "./LinkedList/LinkedList.h"
+
+void print_node_list(Node *node_index)
+{
+	int node_number {1};
+	while( node_index->get_next_node() != nullptr )
+	{
+		std::cout << node_number << "(st/nd) node's value is: " << node_index->get_value() << std::endl;
+		node_index = node_index->get_next_node();
+	}
+}
+
 
 int main()
 {
-	
+	Node first_node(10, nullptr);
+	Node second_node(20, &first_node);
+	Node third_node(30, &second_node);
+	print_node_list(&third_node);
 	std::cout << "test" << std::endl;
-	LinkedList test(10, nullptr);
-
 }
