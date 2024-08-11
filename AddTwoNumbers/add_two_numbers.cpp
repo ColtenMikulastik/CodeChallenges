349953286e79c3121c4b58ba89ee02791eaa988c
Colten Mikulastik <mikulastikc@gmail.com>
2024-08-11 18:55:25 -0500

    messing with implementing reverse num as command line utility

--- AddTwoNumbers/add_two_numbers.cpp
+++ AddTwoNumbers/add_two_numbers.cpp
@@ -1,6 +1,7 @@
 
-// luca's stuff
+#include <iostream>
 
+// luca's stuff
 class Node 
 {
 	private:
@@ -33,8 +34,129 @@ class Node
 
 };
 
+class LinkedList
+{
+    private:
+        // the list will just hold a pointer to the first Node
+        Node *first_node;
+    public:
+	LinkedList(int t_x, Node* t_ptr = nullptr)
+	{
+	    // start list by creating node, and pointing to it
+	    this->first_node = new Node(t_x, t_ptr);
+	    std::cout << "list created!" << std::endl;
+	}
+	void printlist()
+	{
+	    // prints all of the nodes attached to list but more optimised
+	    Node* currentNode = this->first_node;
+	    std::cout << "printing list:" << std::endl;
+	    // loop through list, and print each node
+	    while (currentNode != nullptr)
+	    {
+		// print current node var
+		std::cout << currentNode->get_value() << std::endl;
+		// go to next node
+		currentNode = currentNode->get_next_node();
+	    }
+	    std::cout << "end of list..." << std::endl;
+	}
 
-#include <iostream>
+	void addnode(int t_x)
+	{
+	    // adds a node to the end of the linked list
+	    // create the node that will be added in teh heap
+	    Node* newnode = new Node(t_x, nullptr);
+	    Node* nextnode = this->first_node;
+	    Node* thisnode = nullptr;
+	    // loop through the list
+	    while (nextnode != nullptr)
+	    {
+		thisnode = nextnode; 
+		nextnode = thisnode->get_next_node();
+	    }
+	    // list(node(10)-> node(20)-> null)
+	    // append node to end of list
+	    thisnode->set_next_node(newnode);
+	    // newnode already has nullptr set done!
+	    std::cout << "added new node..." << std::endl;
+
+	}
+
+	void removenode(int x)
+	{
+	    // loop through list, and look for x value and remove it...
+	    Node* nextnode = this->first_node;
+	    
+	    if (nextnode->get_value() == x)
+	    {
+		// then the first node is the value
+		this->first_node = nextnode->get_next_node();
+
+		// reallocate memory at the first node
+		delete nextnode;
+
+		// set nextnode so no longer pointing to unalloc mem
+		nextnode = nullptr;
+	    }
+	    else
+	    {
+		// what is the problem, read 
+		while (nextnode->get_next_node() != nullptr)
+		{
+		    // traverse the linked list
+		    Node* thisnode = nextnode;
+		    nextnode = thisnode->get_next_node();
+		    // when the node is found then remove it
+		    if (nextnode->get_value() == x)
+		    {
+			// grab the node after
+			Node* rejoin = nextnode->get_next_node();
+
+			// this node set beyond next node
+			thisnode->set_next_node(rejoin);
+
+			// after the node is unlinked, then remove the node
+			delete nextnode;
+
+			// continue looping
+			nextnode = rejoin;
+
+			std::cout << "node deleted :" << x << std::endl;
+
+			// final node deletion exception fix, this is not a loop cond for efficiency
+			if (nextnode == nullptr)
+			{
+			    // is last node, so no need to loop anymore
+			    break;
+			}
+		    }
+		}
+	    }
+	}
+
+	~LinkedList()
+	{
+	    // starting at the first node, we delete then move to the next adn then delete
+	    Node* nextNode = this->first_node;
+	    
+	    // loop through list
+	    while (nextNode != nullptr)
+	    {
+		// hold this node in one pointer and the next in the other
+		Node* thisnode = nextNode;
+		nextNode = thisnode->get_next_node();
+		// delete one node, while pointer holds on to next node
+		delete thisnode;
+	    }
+
+	    // make sure that we aren't pointing to heap for no reason
+	    this->first_node = nullptr;
+	    std::cout << "list deleted!" << std::endl;
+	}
+
+};
+
 
 void print_node_list(Node *node_index)
 {
@@ -51,9 +173,8 @@ void print_node_list(Node *node_index)
 
 int main()
 {
-	Node first_node(10, nullptr);
-	Node second_node(20, &first_node);
-	Node third_node(30, &second_node);
-	print_node_list(&third_node);
-	std::cout << "test" << std::endl;
+	LinkedList test(10);
+	test.addnode(20);
+	test.addnode(30);
+	test.printlist();
 }
