99beb4967717d3c8cc3b15b4d6d5ccd6b7b2de77
Colten Mikulastik <mikulastikc@gmail.com>
2024-08-19 21:26:28 -0500

    messing with int to list function

--- AddTwoNumbers/utility_functions.cpp
+++ AddTwoNumbers/utility_functions.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 int pow(int base, int pow);
+void int_to_array(int input, int *output, int size);
 
 
 int pow(int base, int pow)
@@ -15,10 +16,28 @@ int pow(int base, int pow)
     return result;
 }
 
+void int_to_array(int input, int *output, int size)
+{
+    for( int i{}; i <= (size - 1); i++ )
+    {
+	int place = pow( 10, size - 1);
+	output[i] = input / place;
+	input = input - place;
+	size--;
+    }
+
+}
 
 int main() 
 {
-    // here for testing purposes
-    std::cout << pow( 33, 5 );
+    int test = 123;
+    int *ptr = new int[3];
+    int_to_array(test, ptr, 3);
+    for( int i{}; i <= 2; i++ )
+    {
+	std::cout << ptr[i] << std::endl;
+    }
+
+    delete [] ptr;
     return 0;
 }
