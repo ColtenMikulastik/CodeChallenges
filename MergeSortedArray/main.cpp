90088dceda7aa73583ee87df5e927b9ddc853f6b
Colten Mikulastik <mikulastikc@gmail.com>
2024-08-25 18:47:51 -0500

    working on adding sorts

---
+++ MergeSortedArray/main.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+int *selection_sort(int *input, int size)
+{
+	for( int i{}; i < size; i++)
+	{
+		
+
+	}
+	return input;
+}
+
+int main()
+{
+	int *test = new int;
+	test[0] = 3;
+	test[1] = 2;
+	test[2] = 1;
+	selection_sort(test, 3);
+	delete test;
+
+	return 0;
+}
