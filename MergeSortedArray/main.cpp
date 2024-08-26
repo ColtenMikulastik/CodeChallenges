b994c195947b737934ecfe1babcdcba0f31d1381
Colten Mikulastik <mikulastikc@gmail.com>
2024-08-25 19:00:58 -0500

    worked on adding a selection sort function

--- MergeSortedArray/main.cpp
+++ MergeSortedArray/main.cpp
@@ -3,8 +3,22 @@
 
 int *selection_sort(int *input, int size)
 {
-	for( int i{}; i < size; i++)
+	for(int i{}; i < size; i++)
 	{
+		int min_index = i;
+		for(int j = i + 1; j < size; j++)
+		{
+			if( input[j] < input[min_index])
+			{
+				min_index = j;
+			}
+		}
+		
+		if(min_index != i)
+		{
+			
+		}
+
 		
 
 	}
