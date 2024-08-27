e6f28b36012271bbfece618b4ce81138ddbbf51c
Colten Mikulastik <mikulastikc@gmail.com>
2024-08-27 18:34:25 -0500

    working on check all elements for value function

--- IpVersionDetect/main.cpp
+++ IpVersionDetect/main.cpp
@@ -1,9 +1,25 @@
 
 #include <iostream>
+#include <string>
+
+bool has(std::string input, char lookfor)
+{
+	for(int i{}; i < input.size(); i++)
+	{
+		if(input[i] == lookfor)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
 
 int main()
 {
-	std::cout << "Test" << std::endl;
+	std::string test ={"192.168.1.1"};
+	std::cout << has(test, ':') << std::endl;
+	std::cout << test << std::endl;
 	return 0;
 
 }
