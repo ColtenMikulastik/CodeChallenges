67122440bd3c83f1387796e82fd9244603b64b79
Colten Mikulastik <mikulastikc@gmail.com>
2024-08-27 18:26:23 -0500

    changing what I wanna work on, to somethign else

--- MergeSortedArray/main.cpp
+++ MergeSortedArray/main.cpp
@@ -1,37 +1,12 @@
 
 #include <iostream>
 
-int *selection_sort(int *input, int size)
-{
-	for(int i{}; i < size; i++)
-	{
-		int min_index = i;
-		for(int j = i + 1; j < size; j++)
-		{
-			if( input[j] < input[min_index])
-			{
-				min_index = j;
-			}
-		}
-		
-		if(min_index != i)
-		{
-			
-		}
-
-		
-
-	}
-	return input;
-}
-
 int main()
 {
 	int *test = new int;
 	test[0] = 3;
 	test[1] = 2;
 	test[2] = 1;
-	selection_sort(test, 3);
 	delete test;
 
 	return 0;
