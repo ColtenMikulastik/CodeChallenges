6ddeb6c215f468f4c610dd22a85c293384dd55a1
Colten Mikulastik <mikulastikc@gmail.com>
2024-08-27 18:40:35 -0500

    main functionality added

--- IpVersionDetect/main.cpp
+++ IpVersionDetect/main.cpp
@@ -4,22 +4,46 @@
 
 bool has(std::string input, char lookfor)
 {
+	// loop through each element to searhc for character
 	for(int i{}; i < input.size(); i++)
 	{
 		if(input[i] == lookfor)
 		{
+			// return true if found
 			return 1;
 		}
 	}
+	// return false if not
 	return 0;
+}
+
+void ip_ver_check(std::string input)
+{
+	// check for characters
+	if(has(input, ':'))
+	{
+		std::cout << "This is likely IPv6" << std::endl;
+	}
+	else if(has(input, '.'))
+	{
+		std::cout << "This is likely IPv4" << std::endl;
+	}
+	else 
+	{
+		std::cout << "I don't know what this is" << std::endl;
+	}
 }
 
 
 int main()
 {
 	std::string test ={"192.168.1.1"};
-	std::cout << has(test, ':') << std::endl;
-	std::cout << test << std::endl;
+	std::string test2 = {"1050:0000:0000:0000:0005:0600:300c:326b"};
+	std::cout << test + " results are: ";
+	ip_ver_check(test);
+	std::cout << test2 + " results are: ";
+	ip_ver_check(test2);
+
 	return 0;
 
 }
