bcf580b3c2981bf510f3626927438c9d5345786b
Colten Mikulastik <mikulastikc@gmail.com>
2025-01-08 12:35:44 -0600

    functional ipv4 check function

--- IpVersionDetect/main.cpp
+++ IpVersionDetect/main.cpp
@@ -19,20 +19,23 @@ bool has(std::string input, char lookfor)
 	return 0;
 }
 
-void ip_ver_check(std::string input)
+int ip_ver_check(std::string input)
 {
 	// check for characters
 	if(has(input, ':'))
 	{
-		std::cout << "This is likely IPv6" << std::endl;
+		// This is likely IPv6
+		return 6;
 	}
 	else if(has(input, '.'))
 	{
-		std::cout << "This is likely IPv4" << std::endl;
+		// This is likely IPv4
+		return 4;
 	}
 	else 
 	{
-		std::cout << "I don't know what this is" << std::endl;
+		// I don't know what this is
+		return 0;
 	}
 }
 
@@ -50,6 +53,8 @@ std::vector <std::string> split(std::string input, char delim)
 		// remove that data from input string
 		input.erase(0, (next_index + 1));
 	}
+	// put the last bit in there
+	output.push_back(input);
 	return output;
 }
 
@@ -57,9 +62,37 @@ bool is_valid_ipv4(std::string input)
 {
 	// check if this input is a valid ipv4 address
 	std::vector <std::string> split_input = {};
-	split_input = split(input, '.');
-	std::cout << split_input[0] << '\n' << split_input[1] << std::endl;
-
+	// call split function 
+	if(ip_ver_check(input) == 4)
+	{
+		split_input = split(input, '.');
+		if(split_input.size() == 4)
+		{
+			for(int i{}; i < 4; i++)
+			{
+				// check if a valid ipv4 address
+				std::cout << split_input[i] << std::endl;
+				if(!(0 <= stoi(split_input[i]) && stoi(split_input[i]) <= 255))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else if (ip_ver_check(input) == 6)
+	{	
+		split_input = split(input, ':');
+		for(std::string split : split_input)
+		{
+			// check if a valid ipv4 address
+			std::cout << split << std::endl;
+		}
+	}
 
 	return 0;
 }
@@ -69,14 +102,12 @@ int main()
 {
 	std::string test ={"192.168.1.1"};
 	std::string test2 = {"1050:0000:0000:0000:0005:0600:300c:326b"};
-	std::cout << test + " results are: ";
-	ip_ver_check(test);
-	std::cout << test2 + " results are: ";
-	ip_ver_check(test2);
+	std::cout << test + " results are: ipv" << ip_ver_check(test) << std::endl;
+	std::cout << test2 + " results are: ipv" << ip_ver_check(test2) << std::endl;
 	
+	// functional ipv4 checking
 	std::string thing = "12.13.14.15";
-	is_valid_ipv4(thing);
-
+	std::cout << is_valid_ipv4(thing) << std::endl;
 	return 0;
 
 }
